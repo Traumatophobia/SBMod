30ffceda5d35093f2d4a038ecda10475b8cbe966
Jon Marshall <m0gliEz@gmail.com>
2019-02-15 23:25:13 -0800

    Update reference version

--- src/main/java/com/Traumatophobia/prefixmod/Reference.java
+++ src/main/java/com/Traumatophobia/prefixmod/Reference.java
@@ -3,8 +3,8 @@ package com.Traumatophobia.prefixmod;
 public class Reference {
 	//Tells minecraft the details of mod
 	public static final String MOD_ID = "tsb";
-	public static final String NAME = "Skyblock Mod";
-	public static final String VERSION = "0.2";
+	public static final String NAME = "Skyblock And Economy Mod";
+	public static final String VERSION = "2.0";
 	public static final String ACCEPTED_VERSIONS = "[1.12,)";
 	
 	//Specific for proxies
